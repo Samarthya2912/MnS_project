be1ebf8c9ba6068f6300858154b85843495e088e
Samarthya <samarthya55@gmail.com>
2021-10-20 20:59:37 +0530

    finally got it working

--- src/App.js
+++ src/App.js
@@ -7,13 +7,30 @@ import Webcam from 'react-webcam';
 import { createContext, useEffect, useRef } from 'react';
 import {drawMesh} from './utils';
 
-const letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',null]
+const characters = [
+  ['a','b','c','d'],
+  ['e','f','g','h'],
+  ['i','j','k','l'],
+  ['m','n','o','p'],
+  ['q','r','s','t','u'],
+  ['v','w','x','y','z'],
+  ['1','2','3','4','5'],
+  ['6','7','8','9','0'],
+]
 
 function App() {
   const webcamRef = useRef();
   const canvasRef = useRef();
   const videoRef = useRef();
-  let letterIndex = 0;
+  let rowIndex = -1;
+  let columnIndex = -1;
+  let typing = true;
+  let typed = false;
+  let columnUtilized = false;
+  let typeNext = true;
+
+  let rowTraversing = true;
+  let columnTraversing = false;
 
 
   const runFaceLandmarksDetector = async () => {
@@ -50,39 +67,103 @@ function App() {
         input: video
       })
 
-      // console.log(face);
       const ctx = canvasRef.current.getContext("2d");
-      // ctx.drawImage(video, 0,0);
       drawMesh(face, ctx);
       // document.querySelector('h1').innerHTML = face[0].scaledMesh[14][1] - face[0].scaledMesh[13][1] > 10? "mouth is open":"mouth is close";
-      
-      if(face.length > 0) {
-        if(face[0].scaledMesh[374][1] - face[0].scaledMesh[386][1] < 4) {
-          document.querySelector('#eyestatus').innerHTML = "eye is close";
-          if(letters[letterIndex] !== null)
-          document.querySelector('#pad').innerHTML += letters[letterIndex];
-          letterIndex = 26;
-        } else {
-          document.querySelector('#eyestatus').innerHTML = "eye is open";
-          // document.getElementById(`letter_${letterIndex}`).style.color = "black";
+            
+        if(face.length) {        
+          
+          if(face[0].scaledMesh[374][1] - face[0].scaledMesh[386][1] < 4) {
+            document.querySelector('#eyestatus').innerHTML = "eye is close";
+            
+            if(typing && columnTraversing) {
+              document.querySelector('.pad').innerHTML += characters[rowIndex][columnIndex];
+              typing = false;
+              typed = true;
+              
+              columnTraversing = false;
+              rowTraversing = true;
+              columnIndex = 0;
+              clearUI();
+              // rowIndex = 0;
+              
+              setTimeout(() => {
+                typing = true;
+              }, 500);
+            } 
+            
+            if(rowTraversing && typing) {
+              columnTraversing = true;
+              rowTraversing = false;
+
+              typing = false;
+
+              setInterval(() => {
+                typing = true;
+              }, 500);
+            }
+            
+          }else {
+            document.querySelector('#eyestatus').innerHTML = "eye is open";
+          }  
+
+
         }
-      }      
+
     } else {
-      console.log('feed error');
+      console.log('loading...');
     }
   }
   
+
+  const setRowUI = () => {
+    const rows = document.querySelectorAll(".keyboard-row");
+    console.log(rows);
+    rows.forEach(row => {
+      document.getElementById(row.id).style.color = "black";
+    })
+
+    document.getElementById(rows[rowIndex].id).style.color = "blue"
+  }
   
+  const setColumnUI = () => {
+    const chars = document.querySelectorAll(`#row_${rowIndex} .char`);
+
+    chars.forEach(char => {
+      document.getElementById(char.id).style.color = "black"
+    })
+
+    document.getElementById(chars[columnIndex].id).style.color = "red";
+  }
+
+  const clearUI = () => {
+    document.querySelectorAll(".char").forEach(char => {
+      char.style = "";
+    });
+  }
+
   useEffect(runFaceLandmarksDetector, []);
+
   useEffect(() => {
     setInterval(() => {
-      document.getElementById(`letter_${letterIndex}`).style.color = "black";
-      
-      if(letterIndex === 26) letterIndex = 0
-      else letterIndex++;
-      
-      document.getElementById(`letter_${letterIndex}`).style.color = "red";
-    }, 500)
+      if(rowTraversing) {
+        if(rowIndex === -1) rowIndex = 0;
+        else if(rowIndex === characters.length - 1) rowIndex = 0;
+        else rowIndex++;
+
+        setRowUI();
+      } else if(columnTraversing) {
+        if(columnIndex === -1) {
+          columnIndex = 0;
+          if(rowIndex === -1) rowIndex = 0;
+        }
+        else if(columnIndex === characters[rowIndex].length - 1) columnIndex = 0;
+        else columnIndex++;
+
+        setColumnUI();
+      }
+
+    }, 1000);
   }, [])
 
   return (
@@ -92,13 +173,25 @@ function App() {
           <Webcam ref={webcamRef} id="webcam"/>
         </div>
         <h1 id="eyestatus">Eyestatus</h1>
-        <div className="letters">
+
+        <div className="keyboard">
           {
-            letters.map((letter, index) => <h1 key={index} id={`letter_${index}`}>{letter}</h1>)
+            characters.map((row, index) => {
+              return (
+                <div className="keyboard-row" id={`row_${index}`} key={index}>
+                  {
+                    row.map((char, index__) => {
+                      return <h2 className="char" id={char} key={char}>{char}</h2>
+                    })
+                  }
+                </div>
+              )
+            })
           }
         </div>
+
         <div>
-          <h1 id="pad">.</h1>
+          <p className="pad"></p>
           <h1>Writing Pad</h1>
         </div>
     </div>
